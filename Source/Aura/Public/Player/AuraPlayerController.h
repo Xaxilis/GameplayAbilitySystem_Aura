19b4848d21a74325fc13ad3ba39a53c973e85ff6
Xaxilis <bchristophel@hotmail.com>
2023-08-29 13:39:26 -0700

    Completed Section 1-9

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -9,6 +9,8 @@
 
 
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  * 
@@ -21,8 +23,13 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void SetupInputComponent()	override;
 private:
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
